c4b1f6d70357c4e52eea918898e221db1e5a0e28
Matt <goodwin.matthew.eric@gmail.com>
2019-06-18 17:14:37 -0700

    Added Home Button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useEffect } from 'react';
-import 'axios-progress-bar/dist/nprogress.css';
-import './App.scss';
-import TheAppBar from './components/TheAppBar';
-import MonthView from './components/MonthView';
-import { Toolbar } from '@material-ui/core';
-import { setGlobal, useGlobal } from 'reactn';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import moment from 'moment';
-import { Groups } from './data';
-import { loadProgressBar } from 'axios-progress-bar';
+import React, { useEffect } from "react";
+import "axios-progress-bar/dist/nprogress.css";
+import "./App.scss";
+import TheAppBar from "./components/TheAppBar";
+import MonthView from "./components/MonthView";
+import { Toolbar } from "@material-ui/core";
+import { setGlobal, useGlobal } from "reactn";
+import Cookies from "js-cookie";
+import axios from "axios";
+import moment from "moment";
+import { Groups } from "./data";
+import { loadProgressBar } from "axios-progress-bar";
 
-export const LoginUrl = `/login.aspx?ReturnUrl=${encodeURIComponent(document.URL)}`;
+export const LoginUrl = `/login.aspx?ReturnUrl=${encodeURIComponent(
+  document.URL
+)}`;
 
 setGlobal({
-	sidebarOpen: false,
-	showList: false,
-	group: { id: 0, name: '' },
-	groups: [],
-	userId: Cookies.get('LAST_VALID_USER') || 0,
-	myShifts: false,
-	currentDate: moment()
+  sidebarOpen: false,
+  showList: false,
+  group: { id: 0, name: "" },
+  groups: [],
+  userId: Cookies.get("LAST_VALID_USER") || 0,
+  myShifts: false,
+  currentDate: moment()
 });
 
 loadProgressBar();
@@ -30,53 +32,50 @@ export const UseStaticData = false;
 axios.defaults.withCredentials = true;
 
 function App() {
-	const [ group, setGroup ] = useGlobal('group');
-	const [ groups, setGroups ] = useGlobal('groups');
+  const [group, setGroup] = useGlobal("group");
+  const [groups, setGroups] = useGlobal("groups");
 
-	useEffect(() => {
-		fetchGroups();
-	}, []);
+  useEffect(() => {
+    fetchGroups();
+  }, []);
 
-	useEffect(
-		() => {
-			if (groups.length > 0) {
-				console.log('groups has changed and is longer than 0', groups);
-				setGroup({ ...groups[0] });
+  useEffect(() => {
+    if (groups.length > 0) {
+      console.log("groups has changed and is longer than 0", groups);
+      setGroup({ ...groups[0] });
 
-				console.log('set loading to false!');
-			}
-		},
-		[ groups ]
-	);
+      console.log("set loading to false!");
+    }
+  }, [groups]);
 
-	function fetchGroups() {
-		if (UseStaticData) {
-			setGroups([ ...Groups ]);
-			return;
-		}
-		axios
-			.get(`/templates/GetCallboardGroups`)
-			.then((response) => {
-				setGroups([ ...response.data ]);
-			})
-			.catch((error) => {
-				console.log('fetch groups error');
-				console.log(error);
+  function fetchGroups() {
+    if (UseStaticData) {
+      setGroups([...Groups]);
+      return;
+    }
+    axios
+      .get(`/templates/GetCallboardGroups`)
+      .then(response => {
+        setGroups([...response.data]);
+      })
+      .catch(error => {
+        console.log("fetch groups error");
+        console.log(error);
 
-				if (error.response && error.response.status === 401) {
-					alert('You must first login to continue.');
-					window.location.href = LoginUrl;
-				}
-			})
-			.finally(() => {});
-	}
-	return (
-		<React.Fragment>
-			<TheAppBar />
-			<Toolbar />
-			<MonthView />
-		</React.Fragment>
-	);
+        if (error.response && error.response.status === 401) {
+          alert("You must first login to continue.");
+          window.location.href = LoginUrl;
+        }
+      })
+      .finally(() => {});
+  }
+  return (
+    <React.Fragment>
+      <TheAppBar />
+      <Toolbar />
+      <MonthView />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/TheAppBar.js
+++ src/components/TheAppBar.js
@@ -1,92 +1,101 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
 
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import { Slide, Button, Toolbar, AppBar } from '@material-ui/core';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import AppDrawer from './AppDrawer';
-import { useGlobal } from 'reactn';
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import HomeIcon from "@material-ui/icons/Home";
+import { Slide, Button, Toolbar, AppBar } from "@material-ui/core";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import AppDrawer from "./AppDrawer";
+import { useGlobal } from "reactn";
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1
-	},
-	menuButton: {
-		marginRight: theme.spacing(2)
-	},
-	title: {
-		flexGrow: 1,
-		whiteSpace: 'nowrap',
-		textOverflow: 'ellipsis',
-		overflowX: 'hidden'
-	}
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1,
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+    overflowX: "hidden"
+  }
 }));
 
 function HideOnScroll(props) {
-	const { children, window } = props;
-	const trigger = useScrollTrigger();
-	return (
-		<Slide appear={false} direction="down" in={!trigger}>
-			{children}
-		</Slide>
-	);
+  const { children, window } = props;
+  const trigger = useScrollTrigger();
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
 }
 
 HideOnScroll.propTypes = {
-	children: PropTypes.node.isRequired,
-	// Injected by the documentation to work in an iframe.
-	// You won't need it on your project.
-	window: PropTypes.func
+  children: PropTypes.node.isRequired,
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  window: PropTypes.func
 };
 
 function TheAppBar() {
-	const classes = useStyles();
-	const [ open, setOpen ] = useGlobal('sidebarOpen');
-	const [ showList, setShowList ] = useGlobal('showList');
-	const [ myShifts, setMyShifts ] = useGlobal('myShifts');
-	const [ group ] = useGlobal('group');
-	return (
-		<React.Fragment>
-			<HideOnScroll>
-				<AppBar>
-					<Toolbar>
-						<IconButton
-							onClick={() => {
-								if (showList) {
-									setShowList(false);
-								} else {
-									setOpen(true);
-								}
-							}}
-							edge="start"
-							className={classes.menuButton}
-							color="inherit"
-							aria-label="Menu"
-						>
-							{!showList && <MenuIcon />}
-							{showList && <KeyboardArrowLeft />}
-						</IconButton>
-						<Typography variant="h6" className={classes.title}>
-							{group.name}
-						</Typography>
-						<Button
-							onClick={() => {
-								setMyShifts(!myShifts);
-							}}
-							color="inherit"
-						>
-							{!myShifts ? 'All Shifts' : 'My Shifts'}
-						</Button>
-					</Toolbar>
-				</AppBar>
-			</HideOnScroll>
-			<AppDrawer />
-		</React.Fragment>
-	);
+  const classes = useStyles();
+  const [open, setOpen] = useGlobal("sidebarOpen");
+  const [showList, setShowList] = useGlobal("showList");
+  const [myShifts, setMyShifts] = useGlobal("myShifts");
+  const [group] = useGlobal("group");
+  return (
+    <React.Fragment>
+      <HideOnScroll>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              onClick={() => {
+                if (showList) {
+                  setShowList(false);
+                } else {
+                  setOpen(true);
+                }
+              }}
+              edge="start"
+              color="inherit"
+              aria-label="Menu"
+            >
+              {!showList && <MenuIcon />}
+              {showList && <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton
+              color="inherit"
+              className={classes.menuButton}
+              onClick={() => {
+                window.location.href = "/home.aspx";
+              }}
+            >
+              <HomeIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              {group.name}
+            </Typography>
+            <Button
+              onClick={() => {
+                setMyShifts(!myShifts);
+              }}
+              color="inherit"
+            >
+              {!myShifts ? "All Shifts" : "My Shifts"}
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <AppDrawer />
+    </React.Fragment>
+  );
 }
 
 export default TheAppBar;
